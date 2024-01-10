a9ba9a68e89d88123df61f9d25c0ed5517e68fac
Davi Saviano Moraes <87396299+Davi1234567891099@users.noreply.github.com>
2024-01-10 17:54:13 -0300

    cors

--- src/main/java/br/com/davi/config/WebConfig.java
+++ src/main/java/br/com/davi/config/WebConfig.java
@@ -3,13 +3,18 @@ package br.com.davi.config;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_XML;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer.favorParameter(false)
@@ -18,5 +23,14 @@ public class WebConfig implements WebMvcConfigurer{
 		.defaultContentType(APPLICATION_JSON)
 		.mediaType("json", APPLICATION_JSON)
 		.mediaType("xml", APPLICATION_XML);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
 	}
 }
